6106f0c5d7703d6960362ada7924bca52aa2423c
Josue Martinez <josue.mart@live.com.mx>
2017-01-10 12:35:36 -0600

    Se crea la clase alumno y en el programa principal se insertan dos objetos

--- Escuela/Program.cs
+++ Escuela/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Escuela.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ namespace Escuela
         static void Main(string[] args)
         {
             Console.WriteLine("GESTION DE INSTITUCION");
+            var alumno1 = new Alumno() { Id = 1, Nombre = "Juan Carlos", Apellido = "Ruiz", Edad = 36, Telefono = "4491648332", Email="josue.mart@live.com.mx"};
+            var persona1=new Persona() {Id = 1, Nombre = "Freddy", Apellido = "Vega", Edad = 56, Telefono = "568945565"};
+
             Console.ReadLine();
         }
     }
